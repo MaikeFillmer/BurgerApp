09d345a6df62d3745a657da81e6ceb9977b6f752
Maike <Maike.Fillmer@gmail.com>
2016-07-03 18:44:59 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express(); // Tells node that we are creating an "express" server
-var configs = require('./configs.js');
+//var configs = require('./configs.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
